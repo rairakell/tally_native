a13dc673d838d1f8f3de2eea14cf658c3497bacb
lei liu <satbirdd@gmail.com>
2018-02-14 12:50:14 +0800

    add store

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { HomeStack } from './routes';
+import {Provider} from 'react-redux';
+import store from './store';
+
 
 export default class App extends Component {
   render() {
-    return <HomeStack />;
+    return (
+    	<Provider store={store}>
+    		<HomeStack />;
+    	</Provider>
+    ) 
   }
 }
-
-// export default App;
---
+++ reducers/expenses.js
@@ -0,0 +1,3 @@
+export default expenses = (state=[], action) => {
+	return state;
+}
